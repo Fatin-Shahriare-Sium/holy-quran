ae2921da04e2527276774ce93b141b9715c5e6a5
Fatin-Shahriare-Sium <sium1206@gmail.com>
2021-04-08 15:18:06 +0600

    add error page

--- src/component/showSingle.verses.jsx
+++ src/component/showSingle.verses.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import err from '../assets/err.svg'
 import SignalVersesLogic from './signal.verses.logic';
 let SingleVerses=()=>{
     let {id}=useParams()
     return(
-        <SignalVersesLogic id={id}/>
+        // <SignalVersesLogic id={id}/>
+            <div className='container-fluid'>
+               
+               
+                <div className="row error-shower">
+                    <div className="col-md-3 col-offset-2 text">
+                    <h1>Sorry,We are working to fix errors.</h1>
+                    </div>
+                    <div className="col-md-4 col-offset-2">
+                    <img  src={err} alt=''/>
+                    </div>
+                </div>
+                
+                
+            </div>
     )
     
   
