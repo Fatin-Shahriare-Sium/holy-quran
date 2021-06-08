b0c706cc7f53b3209381a77da381682e7661ff87
Fatin-Shahriare-Sium <sium1206@gmail.com>
2021-06-08 16:51:23 +0600

    fix,spell mistake,alhamdulillah

--- src/component/credit.jsx
+++ src/component/credit.jsx
@@ -6,9 +6,9 @@ let Credit=()=>{
             <h2>Credit</h2>
             <div className="container mt-5 credit-container">
                 <p>First of All,by the grace of Allah,I have become able to create this website to make Holy Quran available to all the people in online.
-                    Actually,I used An API which is developed by some greate developers and i also take the idea of FrontEnd of this website from Quran.com .
-                    Thanks to Quran.com's developers to give us a greate API and cool Quran website.  
-                    And,the final thing ,i have to say ,otherwise Credit section will incomplete, that my brother is a greate person who helps encouraging me always time to learn.I used my brother's laptop to build up
+                    Actually,I used An API which is developed by some great developers and i also take the idea of FrontEnd of this website from Quran.com .
+                    Thanks to Quran.com's developers to give us a great API and cool Quran website.  
+                    And,the final thing ,i have to say ,otherwise Credit section will incomplete, that my brother is a great person who helps encouraging me always time to learn.I used my brother's laptop to build up
                     this website.
                     <br/>
                   <span><p> I salute my brother as he helps me every single time giving me All. </p></span>
